a2cae3058ac0ac1f4834a62a1c5cf879f3ee2486
yohei-tarutani <152731552+yohei-tarutani@users.noreply.github.com>
2024-10-05 15:05:43 -0700

    git commit -m "PUS-3 feature: Microphone button (Record and Play button)"

--- components/common/IconButton.js
+++ components/common/IconButton.js
@@ -2,12 +2,17 @@ import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
-const IconButton = ({ icon, color, size, onPress }) => {
+const IconButton = ({ icon, color, size, onPress, recordingUri }) => {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity onPress={onPress ? onPress : navigation.goBack}>
-      <Ionicons name={icon} color={color} size={size} />
+      <Ionicons
+        name={icon}
+        color={color}
+        size={size}
+        disabled={!recordingUri && true}
+      />
     </TouchableOpacity>
   );
 };
--- components/interview/InterviewControllerIcons.js
+++ components/interview/InterviewControllerIcons.js
@@ -1,29 +1,107 @@
 import { useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { Audio } from "expo-av";
 import { StyleSheet, View } from "react-native";
 import IconButton from "../common/IconButton";
 
 const InterviewControllerIcons = () => {
   const navigation = useNavigation();
+  const [recording, setRecording] = useState(null);
+  const [sound, setSound] = useState(null);
+  const [recordingUri, setRecordingUri] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    // Unload sound when component unmounts or when a new sound is played
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const startRecording = async () => {
+    try {
+      // Ask for permissions to access the microphone
+      await Audio.requestPermissionsAsync();
+
+      // Prepare audio recording settings
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording...");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording); // recorded audio file is stored locally
+      setIsRecording(true);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  };
+
+  const stopRecording = async () => {
+    console.log("Stopping recording...");
+    setRecording(undefined);
+    if (recording) {
+      try {
+        // Stops the recording and deallocates the recorder from memory
+        const status = await recording.stopAndUnloadAsync();
+        const uri = recording.getURI(); // Get the URI of the recorded file
+        setRecordingUri(uri);
+        setIsRecording(false);
+        console.log("Recording stopped and stored at:", uri);
+        console.log("Recording status after stopping:", status); // Log the status
+      } catch (error) {
+        console.error("Failed to stop recording", error);
+      }
+    }
+  };
+
+  const playSound = async () => {
+    if (recordingUri) {
+      try {
+        console.log("Loading sound...");
+        const { sound } = await Audio.Sound.createAsync({ uri: recordingUri });
+        setSound(sound);
+        setIsPlaying(true);
+
+        console.log("Playing sound...");
+        await sound.playAsync();
+
+        // Detect when the sound has finished playing
+        sound.setOnPlaybackStatusUpdate((status) => {
+          if (status.didJustFinish) {
+            setIsPlaying(false);
+          }
+        });
+      } catch (error) {
+        console.error("Error playing sound:", error);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <IconButton
           icon="volume-medium-outline"
-          color="black"
+          color={recordingUri ? "black" : "#aaa"}
           size={35}
-          onPress={() => {
-            null;
-          }}
+          onPress={playSound}
+          recordingUri={recordingUri}
         />
       </View>
       <View>
         <IconButton
           icon="mic"
           color="black"
-          size={70}
-          onPress={() => {
-            null;
-          }}
+          size={50}
+          onPress={isRecording ? stopRecording : startRecording}
         />
       </View>
       <View>
@@ -50,7 +128,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: 360,
     marginBottom: 6,
-    // borderWidth: 1,
-    // borderRadius: 12,
   },
 });
