4e7972b5cbc5a8f008e96f5664df9d5bf7064c10
FTW-Khushal <59960215+FTW-Khushal@users.noreply.github.com>
2024-10-30 18:00:02 -0700

    feat: authenticating user

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -5,7 +5,14 @@ export const PATH_INTERVIEW = "interview";
 export const PATH_PROFILE = "profile";
 export const PATH_JOBFINDER = "jobFinder"
 export const PATH_STATMASTER = "starMaster"
+export const PATH_AUTH = "auth"
+
+export const SUB_PATH_FORGOT_PASSWORD = "forgot-password"
 
 export const TYPE_GENERATE_QUESTION = "generate-questions"
 export const TYPE_CATEGORY = "category"
 export const TYPE_ANALYZE_ANSWERS = "analyze-answers"
+export const TYPE_SIGNIN = "signin"
+export const TYPE_SIGNUP = "signup"
+export const TYPE_OTP = "otp"
+export const TYPE_RESET = "reset"
