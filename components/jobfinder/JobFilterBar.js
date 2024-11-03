fa55e6bd12e905242caed0a12ea467ceafc2b25e
shunsaku-sugita <saku.dot@gmail.com>
2024-11-02 18:45:24 -0700

    PUS-345 fix: change the name from Filter by location to Job List

--- components/jobfinder/JobFilterBar.js
+++ components/jobfinder/JobFilterBar.js
@@ -4,7 +4,7 @@ import React from "react";
 const JobFilterBar = (props) => {
   return (
     <View style={styles.container}>
-      <Text onPress={() => props.changeFilter(1)}>Filter By Location</Text>
+      <Text onPress={() => props.changeFilter(1)}>Job List</Text>
       <Text onPress={() => props.changeFilter(0)}>Saved Jobs</Text>
     </View>
   );
@@ -14,9 +14,9 @@ export default JobFilterBar;
 
 const styles = StyleSheet.create({
   container: {
-    display:"flex",
-    flexDirection:"row",
-      gap: 10
+    display: "flex",
+    flexDirection: "row",
+    gap: 10
   },
   text: {
     fontSize: 24,
